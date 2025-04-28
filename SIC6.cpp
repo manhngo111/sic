3ac07c7d32721e8a98eeaa410843c78e0180240a
manhngo111 <ngomanhduc1231@gmail.com>
2025-04-28 20:29:42 +0700

    Add files via upload

---
+++ SIC6.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h> 
+using namespace std;
+int cong(int a,int b,int c)
+{
+	return a+b+c;
+}
+long nhan(int a, int b,int c)
+{
+	return a*b*c;
+}
+int tru(int a,int b,int c)
+{
+	return a-b-c; 
+}
